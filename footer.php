43d0eed78e9acb818cfe8544fe7b26178b7a8fa2
dbthanhbinh <dbthanhbinh@gmail.com>
2018-07-21 18:18:18 +0700

    Footer coppyright

--- footer.php
+++ footer.php
@@ -4,7 +4,28 @@
       <div class="container">  
         <div class="row">      
           <div class="col-lg-12">
-            <p><?= get_theme_mod('setting_copyright') ?></p>
+            <div class="footer-copyright">
+              <span>
+                <?= get_theme_mod('setting_copyright') ?>
+              </span>
+              <?php if ( has_nav_menu ( 'primary' ) ) : ?>
+              <?php 
+                  $defaults = array(
+                      'theme_location'  => 'footer',
+                      'container'       => '',
+                      'container_class' => 'footer-menu',
+                      'container_id'    => 'footer-menu',
+                      'menu_class'      => 'footer-navbar',
+                      'echo'            => true,
+                      'fallback_cb'     => 'wp_page_menu',
+                      'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+                      'depth'           => 2,
+                      'walker'          => false // new menu_walker()
+                  );
+                  wp_nav_menu ($defaults); 
+                  ?>
+              <?php endif; ?>
+            </div>
           </div>
         </div>
       </div>
