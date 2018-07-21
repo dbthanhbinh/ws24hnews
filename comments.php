bfce4964c9a057bcc977cceb5d1188169b651c92
dbthanhbinh <dbthanhbinh@gmail.com>
2018-07-21 21:16:17 +0700

    modify comment form

--- comments.php
+++ comments.php
@@ -55,8 +55,44 @@ if ( post_password_required() ) {
 		<p class="no-comments"><?php _e( 'Comments are closed.', 'ws24h' ); ?></p>
 	<?php
 	endif;
+	// function alter_comment_form_fields($fields){
+	// 	//$fields['author'] = ''; //removes name field
+	// 	$fields['email'] = '';  //removes email field
+	// 	$fields['url'] = '';  //removes website field
+	// 	$fields['cookies'] = '';
+	// 	return $fields;
+	// }	
+	// add_filter('comment_form_default_fields','alter_comment_form_fields');
+	// comment_form([]);
 
-	comment_form();
+
+
+	add_filter( 'comment_form_defaults', 'so16856397_comment_form_defaults', 10, 1 );
+	function so16856397_comment_form_defaults( $defaults )
+	{
+		$defaults['comment_field'] = '<p class="comment-form-comment"> <textarea id="comment" name="comment" placeholder="' . __( 'Comment (*)' ) . '" aria-describedby="form-allowed-tags" aria-required="true" required="required"></textarea></p>';
+		return $defaults;
+	}
+
+	$commenter = wp_get_current_commenter();
+	$req = get_option( 'require_name_email' );
+	$aria_req = ( $req ? " aria-required='true'" : '' );
+
+	$fields =  array(
+		//'comment_field' => '<p class="comment-form-comment"> <textarea id="comment" name="comment" placeholder="' . __( 'Comment' ) . ( $req ? ' (*)' : '' ) .'" aria-describedby="form-allowed-tags" aria-required="true" required="required"></textarea></p>',
+		'author' => '<p class="comment-form-author">' .
+			'<input id="author" name="author" type="text" placeholder="' . __( 'Name' ) . ( $req ? ' (*)' : '' ) .'" value="' . esc_attr( $commenter['comment_author'] ) . '" size="30"' . $aria_req . ' /></p>',
+		'email'  => '<p class="comment-form-email">' .
+			'<input id="email" name="email" type="text" placeholder="' . __( 'Email' ) . ( $req ? ' (*)' : '' ) .'" value="' . esc_attr(  $commenter['comment_author_email'] ) . '" size="30"' . $aria_req . ' /></p>',
+	);
+	
+	$comments_args = array(
+		'fields' =>  $fields,
+		'title_reply'=>'Please give us your valuable comment',
+		'label_submit' => 'Send Comment'
+	);
+	
+	comment_form($comments_args);
 	?>
 
 </div><!-- #comments -->
