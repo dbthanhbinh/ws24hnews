f6bc0bb65d469f49cfdc887c9df8dbd6a2e0f3cb
dbthanhbinh <dbthanhbinh@gmail.com>
2019-07-25 20:05:02 +0700

    init config header

--- assets/js/customjs.js
+++ assets/js/customjs.js
@@ -4,15 +4,19 @@ var mainNavbarId = 'main-navbar';
 var mainNavbarStickyClass = 'main-navbar-fixed';
 var mainNavbar = document.getElementById(mainNavbarId)
 var mainNavbarSticky = mainNavbar.offsetTop;
+var dataRef = mainNavbar.getAttribute('data-ref');
+// if default header (header has banner in top) will be render fixed header 
+// if(dataRef == 'default'){
+    window.addEventListener('scroll', function(e){onScrollHandle()});
+    window.onload = function(){onsLoadHandle()};
+// }
 
-window.addEventListener('scroll', function(e){onScrollHandle()});
-window.onload = function(){onsLoadHandle()};
 function onsLoadHandle() {
     window.scrollTo(window.pageYOffset - 1, 0);
 }
 
 function onScrollHandle() {
-    if(window.innerWidth >= 992 && (window.pageYOffset >= mainNavbarSticky)){
+    if(window.innerWidth >= 992 && (window.pageYOffset > mainNavbarSticky)){
         mainNavbar.classList.add(mainNavbarStickyClass);
     } else {
         mainNavbar.classList.remove(mainNavbarStickyClass);
--- header.php
+++ header.php
@@ -7,32 +7,6 @@
     <?php wp_head()?>
   </head>
   <body <?php body_class(); ?>>
-    <?php if(get_theme_mod('show_top_header') || get_theme_mod('show_header')):?>
-    <div id="top-header" class="top-header">
-      <?php if(get_theme_mod('show_top_header')):?>
-      <div class="container top-header-menu">
-          <ul class="navigation top-bar-menu right">
-              <li class="menu-item "><a href="tel:<?= get_theme_mod('contact_hotline')?>"><i class="fa fa-phone"></i> Hotline: <?= get_theme_mod('contact_hotline')?></a></li>
-          </ul>
-      </div>
-      <?php
-      endif;
-      if(get_theme_mod('show_header')):
-      ?>
-      <div class="container top-header-banner">
-        <div class="row">
-          <div class="top-banner-img col-md-9 col-lg-9 col-sm-9">
-              <div class="top-banner-right">
-                <a href="#"> <?php echo render_mode_attachment_image('top_banner'); ?> </a>
-              </div>
-          </div>
-          <div class="col-md-3 col-lg-3 render-logo">
-              <?= render_logo() ?>
-          </div>
-        </div>
-      </div>
-      <?php endif;?>
-    </div>
-    <?php endif;?>
-    <!-- Navigation -->
-    <?php require_once ('modules/menu/menu.php')?>
+  <?php
+    require_once ('modules/header/header-' . getConfigVersion() . '.php');
+  ?>
